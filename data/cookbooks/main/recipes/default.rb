941509b850591e6ee7eee5457092537b0585f475
Nickleus73 <n.moricet@free.fr>
2014-10-12 19:57:58 +0200

    fix #6 Added git in the configuration and integration of angnode-server

--- data/cookbooks/main/recipes/default.rb
+++ data/cookbooks/main/recipes/default.rb
@@ -1,3 +1,18 @@
+execute "Install git" do
+    command "sudo apt-get -y install git"
+    user "root"
+end
+
+execute "Clone andnode-server" do
+    if File.directory?('./angnode-server')
+        command "cd /var/www/html/node_modules && sudo rm -R angnode-server && git clone https://github.com/nickleus73/angnode-server.git && cd .."
+        user "root"
+    else
+        command "cd /var/www/html/node_modules && git clone https://github.com/nickleus73/angnode-server.git && cd .."
+        user "root"
+    end
+end
+
 execute "Install coffeescript" do
     command "npm install -g coffee-script"
     user "root"
